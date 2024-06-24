3118d85d98e81d62f90293fe7a2abde428bce5b0
cmdright <chridright@tangistudents.org>
2024-06-24 14:33:17 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,27 +20,41 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
+var circle
 
-
-        // TODO 2 : Create a function that draws a circle 
-        
-
-        // TODO 3 / 7 : Call the drawCircle() function 
-
-
+var circles = []
+  // TODO 2 : Create a function that draws a circle 
+       function drawCircle() 
+      { // Code to draw a circle
+       
+        circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+        physikz.addRandomVelocity(circle, canvas, 2.5, 2.5);
+        view.addChild(circle);
+        circles.push(circle);
+        }
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
-        
+        // TODO 3 / 7 : Call the drawCircle() function 
         /* 
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+       
         This Function is called 60 times/second producing 60 frames/second.
         In each frame, for every circle, it should redraw that circle
         and check to see if it has drifted off the screen.         
         */
-        function update() {
+      {  function update() {
             // TODO 4 : Update the circle's position //
-
-            
+            physikz.updatePosition(circles[0]);
+            physikz.updatePosition(circles[1]);
+            physikz.updatePosition(circles[2]);
+            physikz.updatePosition(circles[3]);
+            physikz.updatePosition(circles[4]);
+        }
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
            
 
